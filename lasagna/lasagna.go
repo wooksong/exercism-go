f2550e1a1173f71c7987f0eb031634ac7a7c4178
Wook Song <wook16.song@samsung.com>
2022-07-13 10:56:10 +0900

    Add the solution for 'Gopher's Gorgeous Lasagna'
    For CLI, this exercise's slug is 'lasagna'.

---
+++ lasagna/lasagna.go
@@ -0,0 +1,21 @@
+package lasagna
+
+// TODO: define the 'OvenTime' constant
+const OvenTime = 40
+
+// RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
+func RemainingOvenTime(actualMinutesInOven int) int {
+	return OvenTime - actualMinutesInOven
+}
+
+// PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
+func PreparationTime(numberOfLayers int) int {
+	const preparationTimeForOneLayer = 2
+
+	return numberOfLayers * preparationTimeForOneLayer
+}
+
+// ElapsedTime calculates the total time needed to create and bake a lasagna.
+func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
+	return PreparationTime(numberOfLayers) + actualMinutesInOven
+}
